28231791a1a9510c5ba56f2ff63b4ba00a063f85
Rubens Dos Santos Filho <rubinho.santos@outlook.com>
2018-02-10 11:26:20 -0200

    #2 Criação do cliente HTTP que irá conectar-se ao servidor do bate-papo

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 var app = require('http').createServer();
+var io = require('socket.io')(app);
 
 app.listen(3000, function () {
     console.log('Bem vindo ao bate papo Coffee&Code JOI!');
+});
+
+io.on('connect', function (socket) {
+    console.log('Um novo usuário entrou no bate-papo');
 });
