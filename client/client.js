2367be8f76baa74b8902e83be447d9c29353985c
Rubens Dos Santos Filho <rubinho.santos@outlook.com>
2018-02-10 18:13:59 -0200

    #3 Desenvolvida comunicação entre cliente e servidor

--- client/client.js
+++ client/client.js
@@ -1,1 +1,11 @@
 var socket = require('socket.io-client')('http://localhost:3000');
+var readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+socket.on('connect', function () {
+    readline.question('Informe seu nome de usuário: ', function (name) {
+        socket.emit('register', name);
+    });
+});
