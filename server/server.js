f34833ddcb713098b09c0cfb879f1bd584e37ace
Rubens Dos Santos Filho <rubinho.santos@outlook.com>
2018-02-10 19:53:16 -0200

    #7 Adicionado cores ao chat

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 var app = require('http').createServer();
 var io = require('socket.io')(app);
+var colors = require('colors');
 
 app.listen(3000, function () {
-    console.log('Bem vindo ao bate papo Coffee&Code JOI!');
+    console.log('Bem vindo ao bate papo Coffee&Code JOI!'.bgWhite);
 });
 
 io.on('connect', function (socket) {
     socket.on('register', function (user) {
         socket.user = user;
-        console.log(socket.user.username + ' entrou no bate-papo');
+        console.log((socket.user.username + ' entrou no bate-papo').bgGreen);
     });
 
     socket.on('message', function (message) {
