0c6a9f07b9217217167babc26bf52c36e682d8c7
Winston Spencer Sonnesen <sonnesen@gmail.com>
2018-02-14 21:38:26 -0200

    #6 hello-console - Template Literals

--- hello-console/app.js
+++ hello-console/app.js
@@ -3,6 +3,7 @@ var args = process.argv.slice(2);
 // IIFE (Immediately Invoked Function Expression)
 // Arrow Function
 // Default Parameter Values
+// Template Literals
 ((nome='World') => {
-  console.log('Hello ' + nome + '!');
+  console.log(`Hello ${nome}!`);
 })(args[0]);
