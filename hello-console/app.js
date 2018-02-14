c7bd1161bbfc9b81d671ade14b75e6d135031e3b
Winston Spencer Sonnesen <sonnesen@gmail.com>
2018-02-14 21:37:05 -0200

    #4 hello-console - Arrow Function

--- hello-console/app.js
+++ hello-console/app.js
@@ -1,6 +1,6 @@
 var args = process.argv.slice(2);
 
-// IIFE (Immediately Invoked Function Expression)
-(function sayHello(nome) {
+// Arrow Function
+(nome => {
   console.log('Hello ' + nome + '!');
 })(args[0] === undefined ? 'World' : args[0]);
