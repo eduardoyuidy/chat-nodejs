69ec0a4f94fdddb4836a8ae7ecbb15e1435c80f6
Winston Spencer Sonnesen <sonnesen@gmail.com>
2018-02-14 21:35:14 -0200

    #2 hello-console - Funcao sayHello

--- hello-console/app.js
+++ hello-console/app.js
@@ -1,1 +1,7 @@
-console.log('Hello from console');
+var args = process.argv.slice(2);
+
+function sayHello(nome) {
+  console.log('Hello ' + nome + '!');
+}
+
+sayHello(args[0] === undefined ? 'World' : args[0]);
