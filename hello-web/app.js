426fd49726a6db046f1b72b75a984e2ac658ec60
Winston Spencer Sonnesen <sonnesen@gmail.com>
2018-02-15 01:29:31 -0200

    #3 hello-web - Rotas

--- hello-web/app.js
+++ hello-web/app.js
@@ -1,10 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+
+// configurações
+app.use(bodyParser.urlencoded({extended: false}));
 
 // rotas
 app.get('/', function(req,res,next) {
   res.send('Hello World!');
+});
+app.get('/sayhello/:nome', function(req,res) {
+  res.send('Hello ' + req.params.nome + '!');
 });
 
 /* usa a porta 3000 caso não seja configurada uma outra 
